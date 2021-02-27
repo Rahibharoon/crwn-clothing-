dd2be6385ebe6aede5df8112647be9f94d23f905
Rahibharoon <Rahibharoon@gmail.com>
2021-02-26 22:22:20 -0800

    update  our mune item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component} from 'react';
+import {Route,Switch,Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
 import './App.css';
 
-function App() {
+const HATSPage = (props) => {
+
+  return( 
+  <div>
+<button onClick={()=> props.history.push('/topic')}>Topic</button>
+    <h1>HATS Page</h1>
+    </div>)
+  };
+
+ 
+const App = () =>{
   return (
     <div>
-      <HomePage/>
+       <Switch> 
+        <Route exact path='/'  component={HomePage}/>
+        <Route exact path='/shop/hats' component={HATSPage}/> 
+
+         </Switch> 
     </div>
   );
 }
-
 export default App;
