7d4687173f4063739d393496768d3882b6aeee79
Rahibharoon <Rahibharoon@gmail.com>
2021-02-27 08:15:16 -0800

    updating our app with a shop page,collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, { Component} from 'react';
-import {Route,Switch,Link} from 'react-router-dom';
+import React from 'react';
+import {Route,Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
-
-const HATSPage = (props) => {
-
-  return( 
-  <div>
-<button onClick={()=> props.history.push('/topic')}>Topic</button>
-    <h1>HATS Page</h1>
-    </div>)
-  };
-
- 
 const App = () =>{
   return (
     <div>
        <Switch> 
         <Route exact path='/'  component={HomePage}/>
-        <Route exact path='/shop/hats' component={HATSPage}/> 
-
-         </Switch> 
-    </div>
-  );
-}
+        <Route exact path='/shop' component={ShopPage}/> 
+       </Switch> 
+    </div>);}
 export default App;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+class  ShopPage extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            colletions: SHOP_DATA
+        }
+    }
+
+    render(){
+        const {colletions} = this.state;
+        return(
+            <div className='shop-page'>
+                 {
+                      colletions.map(({id, ...otherCollectionProps}) =>(
+                        <CollectionPreview key={id} {...otherCollectionProps}
+                        />
+                      ))
+                 }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
